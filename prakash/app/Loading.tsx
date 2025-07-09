c12c8f2b57ade51774cb55362e0d3ea616a4160d
yshak <Vyshakn29@gmail.com>
2025-07-09 13:24:04 +0530

    added event

---
+++ prakash/app/Loading.tsx
@@ -0,0 +1,12 @@
+import { div } from 'motion/react-m'
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center bg-neutral-900'>
+      <span className='text-7xl text-white'>PIDICH IRUNNO...</span>
+    </div>
+  )
+}
+
+export default Loading
